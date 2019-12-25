2166740f5562cb2922b8a4d708fa00795c66fc30
Solomon1337 <joe@mein.com>
2019-12-25 13:02:51 +0100

    CPU instructions

--- Cpu.py
+++ Cpu.py
@@ -20,6 +20,17 @@ class Registers():
         self.PC = 0x0100
 
 
+class Stack():
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val):
+        self.stack.append(val)
+
+    def pop(self):
+        return self.stack.pop()
+
+
 class Opcode():
     def __init__(self, name, length, cycles, function):
         self.name = name
@@ -31,55 +42,60 @@ class Opcode():
 class Cpu():
     def __init__(self):
         self.register = Registers()
+        self.stack = Stack()
+        self.memory = Memory()
         self.codes = {
-            0x31: Opcode("LD SP,d16", 3, 12, self.ld_sp_d16),
-            0xaf: Opcode("XOR A", 1, 4, self.xor_a),
-            0x21: Opcode("LD HL,d16", 3, 12, self.ld_hl_d16),
-            0x32: Opcode("LD (HL-),A", 1, 8, self.ld_hl_m_a),
-            0x20: Opcode("JR NZ,r8", 2, 12, self.jr_nz_r8),
-            0x0e: Opcode("LD C,d8", 2, 8, self.ld_c_d8),
-            0x3e: Opcode("LD A,d8", 2, 8, self.ld_a_d8),
-            0xe2: Opcode("LD (C),A", 2, 8, self.ld_c_a),
-            0x0c: Opcode("INC C", 1, 4, self.inc_c),
-            0x77: Opcode("LD (HL),A", 1, 8, self.ld_hl_a),
-            0xe0: Opcode("LDH (a8),A", 2, 12, self.ldh_a8_a),
-            0x11: Opcode("LD DE,d16", 3, 12, self.ld_de_d16),
-            0x1a: Opcode("LD A,(DE)", 1, 8, self.ld_a_de),
-            0xcd: Opcode("CALL a16", 3, 24, self.call_a16),
-            0x13: Opcode("INC DE", 1, 8, self.inc_de),
-            0x7b: Opcode("LD A,E", 1, 4, self.ld_a_e),
-            0xfe: Opcode("CP d8", 2, 8, self.cp_d8),
             0x06: Opcode("LD B,d8", 2, 8, self.ld_b_d8),
-            0x22: Opcode("LD (HL+),A", 1, 8, self.ld_hl_p_a),
-            0x23: Opcode("INC HL", 1, 8, self.inc_hl),
-            0x05: Opcode("DEC B", 1, 4, self.dec_b),
-            0xea: Opcode("LD (a16),A", 3, 16, self.ld_a16_a),
-            0x3d: Opcode("DEC A", 1, 4, self.dec_a),
-            0x0d: Opcode("DEC C", 1, 4, self.dec_c),
-            0x2e: Opcode("LD L,d8", 2, 8, self.ld_l_d8),
-            0x18: Opcode("JR r8", 2, 12, self.jr_r8),
-            0x67: Opcode("LD H,A", 1, 4, self.ld_h_a),
-            0x57: Opcode("LD D,A", 1, 4, self.ld_d_a),
-            0x04: Opcode("INC B", 1, 4, self.inc_b),
-            0x1e: Opcode("LD E,d8", 2, 8, self.ld_e_d8),
-            0xf0: Opcode("LDH A,(a8)", 2, 12, self.ldh_a_a8),
-            0x1d: Opcode("DEC E", 1, 4, self.dec_e),
-            0x24: Opcode("INC H", 1, 4, self.inc_h),
-            0x7c: Opcode("LD A,H", 1, 4, self.ld_a_h),
-            0x28: Opcode("JR Z,r8", 2, 12, self.jr_z_r8),
-            0xe2: Opcode("LD (C),A", 2, 8, self.ld_c_a),
-            0x90: Opcode("SUB B", 1, 4, self.sub_b),
-            0x15: Opcode("DEC D", 1, 4, self.dec_d),
+            0x0e: Opcode("LD C,d8", 2, 8, self.ld_c_d8),
+            0x11: Opcode("LD DE,d16", 3, 12, self.ld_de_d16),
             0x16: Opcode("LD D,d8", 2, 8, self.ld_d_d8),
+            0x1a: Opcode("LD A,(DE)", 1, 8, self.ld_a_de),
+            0x1e: Opcode("LD E,d8", 2, 8, self.ld_e_d8),
+            0x21: Opcode("LD HL,d16", 3, 12, self.ld_hl_d16),
+            0x22: Opcode("LD (HL+),A", 1, 8, self.ld_hl_p_a),
+            0x2e: Opcode("LD L,d8", 2, 8, self.ld_l_d8),
+            0x31: Opcode("LD SP,d16", 3, 12, self.ld_sp_d16),
+            0x32: Opcode("LD (HL-),A", 1, 8, self.ld_hl_m_a),
+            0x3e: Opcode("LD A,d8", 2, 8, self.ld_a_d8),
             0x4f: Opcode("LD C,A", 1, 4, self.ld_c_a),
-            0xc5: Opcode("PUSH BC", 1, 16, self.push_bc),
-            0x17: Opcode("RLA", 1, 4, self.rla),
-            0xc1: Opcode("POP BC", 1, 12, self.pop_bc),
-            0xc9: Opcode("RET", 1, 16, self.ret),
-            0xbe: Opcode("CP (HL)", 1, 8, self.cp_hl),
-            0x7d: Opcode("LD A,L", 1, 4, self.ld_a_l),
+            0x57: Opcode("LD D,A", 1, 4, self.ld_d_a),
+            0x67: Opcode("LD H,A", 1, 4, self.ld_h_a),
+            0x77: Opcode("LD (HL),A", 1, 8, self.ld_hl_a),
             0x78: Opcode("LD A,B", 1, 4, self.ld_a_b),
-            0x86: Opcode("ADD A,(HL)", 1, 4, self.add_a_hl),
+            0x7b: Opcode("LD A,E", 1, 4, self.ld_a_e),
+            0x7c: Opcode("LD A,H", 1, 4, self.ld_a_h),
+            0x7d: Opcode("LD A,L", 1, 4, self.ld_a_l),
+            0xe2: Opcode("LD (C),A", 2, 8, self.ld_c_a),
+            0xea: Opcode("LD (a16),A", 3, 16, self.ld_a16_a),
+            0xe0: Opcode("LDH (a8),A", 2, 12, self.ldh_a8_a),
+            0xf0: Opcode("LDH A,(a8)", 2, 12, self.ldh_a_a8),
+
+            0x04: Opcode("INC B", 1, 4, self.inc_b),
+            0x0c: Opcode("INC C", 1, 4, self.inc_c),
+            0x13: Opcode("INC DE", 1, 8, self.inc_de),
+            0x23: Opcode("INC HL", 1, 8, self.inc_hl),
+            0x24: Opcode("INC H", 1, 4, self.inc_h),
+
+            0x05: Opcode("DEC B", 1, 4, self.dec_b),
+            0x0d: Opcode("DEC C", 1, 4, self.dec_c),
+            0x15: Opcode("DEC D", 1, 4, self.dec_d),
+            0x1d: Opcode("DEC E", 1, 4, self.dec_e),
+            0x3d: Opcode("DEC A", 1, 4, self.dec_a),
+
+            0x18: Opcode("JR r8", 2, 12, self.jr_r8),
+            0x20: Opcode("JR NZ,r8", 2, 12, self.jr_nz_r8),
+            0x28: Opcode("JR Z,r8", 2, 12, self.jr_z_r8),
+
+            0x17: Opcode("RLA", 1, 4, self.rla),
+            0x86: Opcode("ADD A,(HL)", 1, 8, self.add_a_hl),
+            0x90: Opcode("SUB B", 1, 4, self.sub_b),
+            0xaf: Opcode("XOR A", 1, 4, self.xor_a),
+            0xbe: Opcode("CP (HL)", 1, 8, self.cp_hl),
+            0xc1: Opcode("POP BC", 1, 12, self.pop_bc),
+            0xc5: Opcode("PUSH BC", 1, 16, self.push_bc),
+            0xc9: Opcode("RET", 1, 16, self.ret),
+            0xcd: Opcode("CALL a16", 3, 24, self.call_a16),
+            0xfe: Opcode("CP d8", 2, 8, self.cp_d8),
             
 
 
@@ -106,8 +122,8 @@ class Cpu():
     def jr_nz_r8(self):
         pass
 
-    def ld_c_d8(self):
-        pass
+    def ld_c_d8(self, location):
+        self.register.C = self.memory.read(location)[0]
 
     def ld_a_d8(self):
         pass
@@ -116,7 +132,7 @@ class Cpu():
         pass
 
     def inc_c(self):
-        pass
+        self.register.C += 1
 
     def ld_hl_a(self):
         pass
@@ -124,17 +140,17 @@ class Cpu():
     def ldh_a8_a(self):
         pass
 
-    def ld_de_d16(self):
-        pass
+    def ld_de_d16(self, location):
+        self.register.DE = self.memory.read(location)[0:2]
 
     def ld_a_de(self):
-        pass
+        self.register.A = self.memory.read(self.register.DE)[0]
 
     def call_a16(self):
         pass
 
     def inc_de(self):
-        pass
+        self.register.DE += 1
 
     def ld_a_e(self):
         pass
@@ -142,26 +158,26 @@ class Cpu():
     def cp_d8(self):
         pass
 
-    def ld_b_d8(self):
-        pass
+    def ld_b_d8(self, location):
+        self.register.B = self.memory.read(location)[0]
 
     def ld_hl_p_a(self):
         pass
 
     def inc_hl(self):
-        pass
+        self.register.HL += 1
 
     def dec_b(self):
-        pass
+        self.register.B -= 1
 
     def ld_a16_a(self):
         pass
 
     def dec_a(self):
-        pass
+        self.register.A -= 1
 
     def dec_c(self):
-        pass
+        self.register.C -= 1
 
     def ld_l_d8(self):
         pass
@@ -176,7 +192,7 @@ class Cpu():
         pass
 
     def inc_b(self):
-        pass
+        self.register.B += 1
 
     def ld_e_d8(self):
         pass
@@ -185,10 +201,10 @@ class Cpu():
         pass
 
     def dec_e(self):
-        pass
+        self.register.E -= 1
 
     def inc_h(self):
-        pass
+        self.register.H += 1
 
     def ld_a_h(self):
         pass
@@ -200,19 +216,19 @@ class Cpu():
         pass
 
     def dec_d(self):
-        pass
+        self.register.D -= 1
 
-    def ld_d_d8(self):
-        pass
+    def ld_d_d8(self, location):
+        self.register.D = self.memory.read(location)[0]
 
     def push_bc(self):
-        pass
+        self.stack.push(self.register.BC)
 
     def rla(self):
         pass
 
     def pop_bc(self):
-        pass
+        self.register.BC = self.stack.pop()
 
     def ret(self):
         pass
--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import sys
 from RomUtils import read_file
-import Rom
+from Memory import Memory
+from Cpu import Cpu
 
 
 def main():
-    romfile = sys.argv[1]
-    bindata = read_file(romfile)
-    rom = Rom.Rom(bindata)
-    print(rom.get_title())
+    #romfile = sys.argv[1]
+    # read_file(romfile)
+    # rom = Rom.Rom(bindata)
+    #print(rom.get_title())
+
+    bindata = Memory().bios
+    cpu = Cpu()
+    a = cpu.codes[0x1a].function()
+    print(a)
 
 
 if __name__ == "__main__":
